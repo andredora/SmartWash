b83d181b3ec5edfa53c7c030a4e3b1685ea26d9b
carolinaspsilva <carolinaspsilva@ua.pt>
2024-05-08 19:14:51 +0100

    mapa bonito

--- smartwash/src/components/StaticMap.jsx
+++ smartwash/src/components/StaticMap.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const StaticMap = () => {
   return (
-    <div style={{ width: '100%', height: '600px', overflow: 'auto' }}>
-      <img src='src/assets/MapaAveiro.png' // Alterar o caminho da imagem para 'src/assets/MapaAv
+    <div style={{ width: '100%', height: '500px', overflow: 'auto', marginTop: '15px' }}>
+      <img src='src/assets/MapaAveiro2.png' // Alterar o caminho da imagem para 'src/assets/MapaAv
         alt="Mapa Estático"
         style={{ width: '100%', height: 'auto' }}
       />
--- smartwash/src/pages/Search.jsx
+++ smartwash/src/pages/Search.jsx
@@ -34,27 +34,28 @@ export default function Search() {
   };
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1><b>Mapa do Transporte</b></h1>
-      <label className="input input-bordered input-info flex items-center gap-2">
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <div style={{fontSize: '24px', marginBottom: '20px'}}><h1><b>Mapa do Transporte</b></h1>
+      </div>
+      <label style={{marginTop: '2px'}} className="input input-bordered input-info flex items-center gap-2">
         <input ref={originRef} type="text" className="grow" placeholder="Insira a Origem" />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
       </label>
-      <label className="input input-bordered input-info flex items-center gap-2">
+      <label style={{marginTop: '10px'}} className="input input-bordered input-info flex items-center gap-2">
         <input ref={destinationRef} type="text" className="grow" placeholder="Insira o destino" />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
       </label>
      
       <StaticMap /> {/* Adicionando o componente StaticMap aqui */}
 
-      <div style={{ marginTop: '1px' }}>
+      <div style={{ marginTop: '10px' }}>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleReserveTrip}>Reservar Viagem</button>
         {showReservationPopup && (
           <ReservationPopup onClose={() => setShowReservationPopup(false)} />
         )}
       </div>
 
-      <div style={{ marginTop: '1px' }}>
+      <div style={{ marginTop: '5px', marginBottom: '10px' }}>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleTakeToAnyLaundry}>Levar à Lavandaria Disponível</button>
       </div>
     </div>
