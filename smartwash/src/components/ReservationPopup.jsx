d22c9d846f3aecd9df5722b485586983bf816da8
carolinaspsilva <carolinaspsilva@ua.pt>
2024-05-08 23:13:57 +0100

    pop up reserva

--- smartwash/src/components/ReservationPopup.jsx
+++ smartwash/src/components/ReservationPopup.jsx
@@ -1,12 +1,14 @@
-import { faBorderAll, faBorderStyle } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // Importe o estilo padrão do DatePicker
 
 const ReservationPopup = ({ onClose }) => {
   const [selectedLaundry, setSelectedLaundry] = useState(null);
   const [selectedTime, setSelectedTime] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date()); // Estado para armazenar a data selecionada
   const [quantity, setQuantity] = useState(1);
   const [washCycles, setWashCycles] = useState(1);
-  const [isFormValid, setIsFormValid] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(true); // Alterado para true inicialmente
 
   const handleLaundrySelection = (laundry) => {
     setSelectedLaundry(laundry);
@@ -14,80 +16,92 @@ const ReservationPopup = ({ onClose }) => {
 
   const handleReservation = () => {
     // Check if all required fields are filled
-    if (selectedLaundry && selectedTime && quantity && washCycles) {
+    if (selectedLaundry && selectedTime && quantity && washCycles && selectedDate) {
       // Add logic to make reservation
       console.log('Reservation made:', {
         laundry: selectedLaundry,
         time: selectedTime,
         quantity,
         washCycles,
+        date: selectedDate // Adicionado a data na saída do console.log
       });
       onClose(); // Close the popup after reservation is made
     } else {
-        setIsFormValid(false); // Define o estado como falso para mostrar a mensagem de erro
+      setIsFormValid(false); // Define o estado como falso para mostrar a mensagem de erro
     }
   };
 
   const handleTimeChange = (e) => {
     setSelectedTime(e.target.value);
   };
+
   return (
-    <div className="popup" sytle= {{ border: '2px solid #000'}}>
-      <div className="popup-inner" >
-        <h2 style={{fontSize: '20px'}}><b>Reservar Lavanderia</b></h2>
-        <div><label style={{fontsize: '14px'}}>
+    <div className="popup" style={{ border: '2px solid #000' }}>
+      <div className="popup-inner">
+        <h2 style={{ fontSize: '20px' }}><b>Reservar Lavanderia</b></h2>
+        <div><label style={{ fontSize: '16px' }}>
           * Quantidade de Roupa:
-          <input style={{color: 'currentcolor', fontsize: '14px', width: '200px'}} type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-        </label>
-        </div>
-        <div><label >
+          <input style={{ color: 'currentcolor', fontSize: '14px', width: '200px' }} type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+        </label></div>
+        <div><label>
           * Nº de Lavagens Necessárias:
-          <input style={{color: 'currentcolor', fontsize: '14px', width: '160px'}} type="number" value={washCycles} onChange={(e) => setWashCycles(e.target.value)} />
+          <input style={{ color: 'currentcolor', fontSize: '14px', width: '160px' }} type="number" value={washCycles} onChange={(e) => setWashCycles(e.target.value)} />
         </label></div>
         <div><label>
           * Horário:
-          <input style={{color: 'currentcolor', fontsize: '14px', width: '300px'}} type="time" value={selectedTime} onChange={(handleTimeChange) }/>
+          <input style={{ color: 'currentcolor', fontSize: '14px', width: '300px' }} type="time" value={selectedTime} onChange={(e) => handleTimeChange(e)} />
         </label></div>
+        <div style={{ width: '310px'}}>
+          <label>
+            * Data:
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="dd/MM/yyyy"
+              style={{ color: 'currentcolor', fontSize: '14px', width: '300px' }}
+            />
+          </label>
+        </div>
         <div style={{ width: '440px', margin: '0 auto', marginBottom: '5px', textAlign: 'center' }}>
-      <h3 style={{ color: 'currentcolor', fontSize: '16px' }}>
-        * Escolha uma Lavanderia Disponível no Momento:
-      </h3>
-      <div>
-        <button 
-          className={`rounded-full ${selectedLaundry === 'A Lavandeira' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
-          onClick={() => handleLaundrySelection('A Lavandeira')}
-          style={{ cursor: 'pointer', marginBottom: '5px' }}
-        >
-          A Lavandeira
-        </button>
-      </div>
-      <div>
-        <button 
-          className={`rounded-full ${selectedLaundry === 'LavEsec lavandaria' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
-          onClick={() => handleLaundrySelection('LavEsec lavandaria')}
-          style={{ cursor: 'pointer', marginBottom: '5px' }}
-        >
-          LavEsec lavandaria
-        </button>
-      </div>
-      <div>
-        <button 
-          className={`rounded-full ${selectedLaundry === 'Clean2you' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
-          onClick={() => handleLaundrySelection('Clean2you')}
-          style={{ cursor: 'pointer', marginBottom: '5px' }}
-        >
-          Clean2you
-        </button>
-      </div>
+          <h3 style={{ color: 'currentcolor', fontSize: '16px' }}>
+            * Escolha uma Lavanderia Disponível no Momento:
+          </h3>
+          <div>
+            <button
+              className={`rounded-full ${selectedLaundry === 'A Lavandeira' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
+              onClick={() => handleLaundrySelection('A Lavandeira')}
+              style={{ cursor: 'pointer', marginBottom: '5px' }}
+            >
+              A Lavandeira
+            </button>
+          </div>
+          <div>
+            <button
+              className={`rounded-full ${selectedLaundry === 'LavEsec lavandaria' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
+              onClick={() => handleLaundrySelection('LavEsec lavandaria')}
+              style={{ cursor: 'pointer', marginBottom: '5px' }}
+            >
+              LavEsec lavandaria
+            </button>
+          </div>
+          <div>
+            <button
+              className={`rounded-full ${selectedLaundry === 'Clean2you' ? 'bg-green-700' : 'bg-green-500'} hover:bg-green-700 text-white font-bold py-2 px-4 uppercase`}
+              onClick={() => handleLaundrySelection('Clean2you')}
+              style={{ cursor: 'pointer', marginBottom: '5px' }}
+            >
+              Clean2you
+            </button>
           </div>
         </div>
-        {/* Adicionando a mensagem de erro */}
-        {!isFormValid && <p style={{ color: 'red' }}>* Por favor, preencha todos os campos obrigatórios.</p>}
-        <div>
-        <div><button className="bg-blue-800 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full" style={{marginTop: '5px', marginBottom: '5px'}} onClick={handleReservation}>Reservar</button>
-        </div>
-        <div><button className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" style={{marginTop: '5px', marginBottom: '5px'}} onClick={onClose}>Fechar</button>
       </div>
+      {/* Adicionando a mensagem de erro */}
+      {!isFormValid && <p style={{ color: 'red' }}>* Por favor, preencha todos os campos obrigatórios.</p>}
+      <div>
+        <div><button className="bg-blue-800 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full" style={{ marginTop: '5px', marginBottom: '5px' }} onClick={handleReservation}>Reservar</button>
+        </div>
+        <div><button className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" style={{ marginTop: '5px', marginBottom: '5px' }} onClick={onClose}>Fechar</button>
+        </div>
       </div>
     </div>
   );
--- smartwash/src/pages/Search.jsx
+++ smartwash/src/pages/Search.jsx
@@ -45,14 +45,13 @@ export default function Search() {
         <input ref={destinationRef} type="text" className="grow" placeholder="Insira o destino" />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
       </label>
-     
+    
       <StaticMap /> {/* Adicionando o componente StaticMap aqui */}
-
-      <div style={{ marginTop: '10px' }}>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleReserveTrip}>Reservar Viagem</button>
-        {showReservationPopup && (
+      {showReservationPopup && (
           <ReservationPopup onClose={() => setShowReservationPopup(false)} />
         )}
+      <div style={{ marginTop: '10px' }}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleReserveTrip}>Reservar Viagem</button>
       </div>
 
       <div style={{ marginTop: '5px', marginBottom: '10px' }}>
