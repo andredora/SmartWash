93567408dfa2364cb1a3b4f0147073c88df73d2a
carolinaspsilva <carolinaspsilva@ua.pt>
2024-05-09 03:50:40 +0100

    linkar home botão

--- smartwash/src/components/PurchaseSuccessPopup.jsx
+++ smartwash/src/components/PurchaseSuccessPopup.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PurchaseSuccessPopup = ({ onClose }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-md shadow-md">
         <h2 className="text-lg font-bold mb-4">Pagamento e Reserva efetuados com sucesso</h2>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full block mx-auto" onClick={onClose}>Fechar</button>
+        <Link to="/">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full block mx-auto" onClick={onClose}>Fechar</button>
+        </Link>
       </div>
     </div>
   );
