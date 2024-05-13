101807b7ce931d8c8a44b1ca5ad87edcaaaa51be
carolinaspsilva <carolinaspsilva@ua.pt>
2024-05-13 23:14:00 +0100

    mapa transporte final

--- smartwash/src/components/InputsTrans.jsx
+++ smartwash/src/components/InputsTrans.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 
 const InputsTrans = () => {
-  const [origem, setOrigem] = useState('DETI - Departamento de Eletrónica Tecnologia e Informática');
-  const [destino, setDestino] = useState('A Lavandeira');
+  const [origem, setOrigem] = useState('A Lavandeira');
+  const [destino, setDestino] = useState('DETI - Departamento de Eletrónica, Telecomunicações e Informática');
 
   return (
-    <div style={{ width: '100%', maxWidth: '500px', margin: '0 auto' }}> {/* Define largura máxima e centraliza horizontalmente */}
-      <div style={{ marginTop: '2px', marginInline:'100px' }} className="input input-bordered input-info flex items-center gap-2">
+    <div style={{ width: '100%', maxWidth: '500px', margin: '0 auto' }}>
+      <div style={{ marginTop: '2px', marginInline: '50px' }} className="input input-bordered input-info flex items-center gap-2">
+        <div style={{ fontWeight: 'bold' }}>From:</div>
         <div className="grow">{origem}</div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-          <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
-        </svg>
       </div>
-      <div style={{ marginTop: '10px', marginInline:'100px' }} className="input input-bordered input-info flex items-center gap-2">
+      <div style={{ marginTop: '10px', marginInline: '50px' }} className="input input-bordered input-info flex items-center gap-2">
+        <div style={{ fontWeight: 'bold' }}>To:</div>
         <div className="grow">{destino}</div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-          <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
-        </svg>
       </div>
     </div>
   );
--- smartwash/src/components/StaticMap.jsx
+++ smartwash/src/components/StaticMap.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Uber from '../assets/Uber.jpg';
+import trajeto from '../assets/Trajeto.jpg';
 
 
 const StaticMap = () => {
   return (
     <div style={{ width: '100%', height: '500px', overflow: 'auto', marginTop: '15px' }}>
-        <img src={Uber} alt="Mapa da viagem" 
+        <img src={trajeto} alt="Mapa da viagem" 
         style={{ width: 'auto', height: '100%', display: 'block', margin: '0 auto'}}
       />
     </div>
